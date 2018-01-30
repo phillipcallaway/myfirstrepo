fb8304b0da08cdc7d891647fa978f066ce47a2e8
root <root@ays-server>
2018-01-30 16:32:21 +0000

    Auto Commit By AYS

---
+++ actors/g8client/actions.py
@@ -0,0 +1,140 @@
+def input (job):
+    args = job.model.args
+    if "account" not in args or args["account"].strip() == "":
+        args['account'] = args["login"]
+    login = args.get('login', '')
+    password = args.get('password', '')
+    jwt = args.get('jwt', '')
+    if login == '' and password == '' and jwt == '':
+        raise j.exceptions.Input("Either username/password or jwt should be entered for %s" % job.service)
+    return args
+    
+
+def processChange (job):
+    service = job.service
+    args = job.model.args
+    category = args.pop('changeCategory')
+    if category == 'dataschema':
+        for key, value in args.items():
+            setattr(service.model.data, key, value)
+    service.save()
+    
+
+def init (job):
+    pass
+    
+
+def install (job):
+    pass
+    
+
+def stop (job):
+    pass
+    
+
+def start (job):
+    pass
+    
+
+def monitor (job):
+    pass
+    
+
+def halt (job):
+    pass
+    
+
+def check_up (job):
+    pass
+    
+
+def check_down (job):
+    pass
+    
+
+def check_requirements (job):
+    pass
+    
+
+def cleanup (job):
+    pass
+    
+
+def data_export (job):
+    pass
+    
+
+def data_import (job):
+    pass
+    
+
+def uninstall (job):
+    pass
+    
+
+def removedata (job):
+    pass
+    
+
+def consume (job):
+    pass
+    
+
+def action_pre_ (job):
+    pass
+    
+
+def action_post_ (job):
+    pass
+    
+
+def init_actions_ (service, args):
+    '''
+    this needs to returns an array of actions representing the depencies between actions.
+Looks at ACTION_DEPS in this module for an example of what is expected
+
+    '''
+    
+    # some default logic for simple actions
+    
+    action_required = args.get('action_required')
+    
+    if action_required in ['stop', 'uninstall']:
+        for action_name, action_model in service.model.actions.items():
+            if action_name in ['stop', 'uninstall']:
+                continue
+            if action_model.state == 'scheduled':
+                action_model.state = 'new'
+    
+    if action_required in ['install']:
+        for action_name, action_model in service.model.actions.items():
+            if action_name in ['uninstall', 'stop'] and action_model.state == 'scheduled':
+                action_model.state = 'new'
+    
+    
+    if action_required == 'stop':
+        if service.model.actionsState['start'] == 'sheduled':
+            service.model.actionsState['start'] = 'new'
+    
+    if action_required == 'start':
+        if service.model.actionsState['stop'] == 'sheduled':
+            service.model.actionsState['stop'] = 'new'
+    
+    service.save()
+    
+    return {
+        'init': [],
+        'install': ['init'],
+        'start': ['install'],
+        'monitor': ['start'],
+        'stop': [],
+        'delete': ['uninstall'],
+        'uninstall': ['stop'],
+    }
+    
+    
+
+def delete (job):
+    job.service.delete()
+    
+
